90f5eb713e2d3dae21691aa910aff58f1d3d4f6a
kartik-lamkhade <kartiklamkhade7@gmail.com>
2026-05-05 15:53:20 +0530

    Update try.py

--- try.py
+++ try.py
@@ -44,8 +44,8 @@ if st.button("start coversation"):
         chain = template | model | parser
         out = chain.invoke({"context":a,"question":Query})
         st.write(out)
-    except:
-        st.write("uplode file")
+    except Exception as e:
+        st.write(f"Error {e}")
 else:
     st.write("NUN")
 
