e5499abf82fe1e91f664995b98aa021c40698372
kartik-lamkhade <kartiklamkhade7@gmail.com>
2026-05-05 15:42:25 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,55 @@
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader, PDFPlumberLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_core.documents import Document
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import tempfile
+
+st.title("Document answer")
+parser = StrOutputParser()
+model = ChatHuggingFace(llm=HuggingFaceEndpoint(model="Qwen/Qwen2.5-Coder-7B-Instruct"))
+emb_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+file = st.file_uploader("Uplode file here",type='pdf')
+if st.button("start coversation"):
+    if file is not None:
+        try:
+            with tempfile.NamedTemporaryFile(delete=False,suffix='.pdf') as tmp:
+                temp_path = tmp.name
+            loader = PDFPlumberLoader(temp_path)
+            document = loader.load()
+            spliter = RecursiveCharacterTextSplitter(chunk_size=100,chunk_overlap=20)
+            docs = []
+            for d in document:
+                docs.extend(spliter.split_text(d.page_content))
+            documents = [Document(page_content=doc) for doc in docs]
+            vectore_store = FAISS.from_documents(
+                documents=documents,
+                embedding=emb_model
+            )
+        
+            Query = st.text_input("Enter query here")
+            output = vectore_store.similarity_search(Query,k=5)
+            def add(doc):
+                a = ""
+                for i in doc:
+                    a += i.page_content + "\n"
+                return a
+            a = add(output)
+            template = PromptTemplate(template="give me answer for the question based on the following context: {context} and question: {question}",
+                                    input_variables=["context","question"])
+            chain = template | model | parser
+            out = chain.invoke({"context":a,"question":Query})
+            st.write(out)
+        except:
+            st.write("uplode file")
+else:
+    st.write("NUN")
+
+
+
+
